8081a2eef10f608255791114890f3c0ade36e806
jayakanth45 <jayakanthreddykancharla@gmail.com>
2024-07-08 22:07:27 +0530

    Time: 0 ms (100.00%), Space: 8 MB (47.65%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+  vector<int> sumup(int n){
+        int res=1;
+      vector<int> temp;
+      temp.push_back(1);
+        for(int i=1;i<n;i++){
+            res=res*(n-i);
+            res/=(i);
+            temp.push_back(res);
+        }
+        return temp;
+    }
+    void giveans(vector<vector<int>> &ans,int n){
+       for(int i=1;i<=n;i++){
+           ans.push_back(sumup(i));
+       }
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        giveans(ans,numRows);
+        return ans;
+        
+    }
+};
