4eb5249fc1407a9ae8e914e8d181dbe53a33868f
jayakanth45 <jayakanthreddykancharla@gmail.com>
2024-07-11 12:05:54 +0530

    Time: 3 ms (49.64%), Space: 7.5 MB (84.88%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
-       stack<char> st;
-        string temp="",ans="";
-        bool flag=0;
-        for(int i=0;i<s.size();i++){
-          if(s[i]=='('){
-              flag=1;
-              st.push('(');
-          }
-            else if(s[i]==')'){
-                while(st.top()!='(' && !st.empty()){
-                    temp=temp+st.top();
-                    st.pop();
-                }
-                
-                
-                    st.pop();
-                if(st.empty()){
-                    ans+=temp;
-                    flag=0;
-                }
-                if(flag){
-                for(int k=0;k<temp.size();k++){
-                    st.push(temp[k]);
-                }
-                }
-                
-                temp="";
-            }
-           
-            else if(flag){
-                st.push(s[i]);
-            }
-            else if(st.empty()){
-                ans+=s[i];
+     stack<int> st;
+
+       for(int i=0;i<s.size();i++){
+        if(s[i]=='('){
+            st.push(i);
+        }
+        else if(s[i]==')'){
+            int k = st.top();
+            st.pop();
+            reverse(s.begin()+k+1,s.begin()+i);
+        }
+       }
+       string result;
+        for (char c : s) {
+            if (c != '(' && c != ')') {
+                result += c;
             }
         }
-        return ans;
+
+    return result; 
         
     }
 };
