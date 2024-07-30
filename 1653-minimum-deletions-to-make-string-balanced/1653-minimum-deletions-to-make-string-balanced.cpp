4172619819e3fb921641791bd80a2bcbe05281d3
jayakanth45 <jayakanthreddykancharla@gmail.com>
2024-07-30 13:53:02 +0530

    Time: 117 ms (21.21%), Space: 24.9 MB (50.24%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -4,12 +4,10 @@ public:
         stack<char> st;
         int count=0;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='b' && st.empty()){
+            if(s[i]=='b'){
                 st.push('b');
             }
-            else if(s[i]=='b'){
-                st.push('b');
-            }
+           
             else if(s[i]=='a' && !st.empty() && st.top()=='b'){
                 count++;
                 st.pop();
