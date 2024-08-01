066a770ad8b6fee4251e05073cd0dff5031ba0fb
jayakanth45 <jayakanthreddykancharla@gmail.com>
2024-08-01 12:29:23 +0530

    Time: 8 ms (47.01%), Space: 17.6 MB (16.71%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -3,14 +3,9 @@ public:
     int countSeniors(vector<string>& details) {
         int count=0;
         for(int i=0;i<details.size();i++){
-            string str=details[i];
-           if(int(str[11])-48>6)
-               count++;
-            else if(int(str[11])-48==6){
-                if(str[12]!='0'){
-                    count++;
-                }
-            }
+            if(details[i][11]=='7' || details[i][11]=='8' || details[i][11]=='9' || (details[i][11]=='6' && details[i][12]!='0'))
+                count++;
+            
         }
         return count;
     }
