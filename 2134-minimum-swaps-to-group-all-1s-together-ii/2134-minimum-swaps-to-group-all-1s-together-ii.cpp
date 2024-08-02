8f41e4298ba6077e935a6f5383e86759994dbd3d
jayakanth45 <jayakanthreddykancharla@gmail.com>
2024-08-02 10:36:31 +0530

    Time: 73 ms (35.76%), Space: 83.1 MB (57.56%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -4,18 +4,18 @@ public:
         
         int n=nums.size();
         int ones=0;
-        // Count the number of 1s in the array
+     
         for(int i=0;i<n;i++){
             if(nums[i]==1){
                 ones++;
             }
         }
-        // Edge case: if there are no 1s or all are 1s, no swaps needed
+       
         if(ones==n || ones==0){
             return 0;
         }
 
-        // First window: count the number of 0s in the first window of size `ones`
+       
         int minSwaps=0;
         int swaps=0;
         for(int i=0;i<ones;i++){
@@ -25,14 +25,14 @@ public:
             }
         }
 
-        // Sliding window
+       
         int i=0,j=ones;
         while(i<n){
-            // Remove the leftmost element
+            
             if(nums[i]==0){
                 swaps--;
             }
-            // Add the next element in the array to the window
+          
             if(nums[j]==0){
                 swaps++;
             }
@@ -40,7 +40,7 @@ public:
             minSwaps=min(minSwaps,swaps);
 
             i++;
-            j=(j+1)%n;// Use modulo to wrap around the array
+            j=(j+1)%n;
         }
 
         return minSwaps;
