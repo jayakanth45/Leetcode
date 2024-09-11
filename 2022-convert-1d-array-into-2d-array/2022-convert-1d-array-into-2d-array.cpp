dec60f21debeccde28940943979352cd004d94e4
jayakanth45 <jayakanthreddykancharla@gmail.com>
2024-09-12 01:00:42 +0530

    Time: 98 ms (8.08%), Space: 92.7 MB (25.02%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -8,15 +8,12 @@ public:
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if( k>original.size()-1)
-                    return {};
-                
+              
                 ans[i][j]=original[k];
                 k++;
             }
         }
-        if(k!=original.size())
-            return {};
+        
         
         return ans;
     }
