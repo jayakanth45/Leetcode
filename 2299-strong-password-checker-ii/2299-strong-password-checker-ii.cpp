ac9b94c2de28ce1a38fa844028d928b0a98a1d2c
jayakanth45 <jayakanthreddykancharla@gmail.com>
2024-09-12 21:11:36 +0530

    Time: 3 ms (50.76%), Space: 7.6 MB (43.18%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool strongPasswordCheckerII(string password) {
+        int count=0;
+        bool flag1=0,flag2=0,flag3=0,flag4=0;
+        if(password.size()<8){
+            return false;
+        }
+        for(int i=0;i<password.size();i++){
+            char x=password[i];
+            if(i!=password.size()-1 && password[i]==password[i+1]){
+                return false;
+            }
+           else if(isalpha(x) && islower(x) && !flag1){
+                count++;
+                flag1=1;
+            }
+            else if(isalpha(x) && isupper(x) && !flag2){
+                count++;
+                flag2=1;
+            }
+            else if(isdigit(x) && !flag3){
+                count++;
+                flag3=1;
+            }
+            else if(!isalpha(x) && !isdigit(x)){
+                if(!flag4){
+                count++;
+                    flag4=1;
+                }
+                
+            }
+        }
+        
+        if(count==4){
+            return true;
+        }
+        return false;
+        
+    }
+};
