cba5a345fd64e3ee2b331d5433e035ffbc54fb75
jayakanth45 <jayakanthreddykancharla@gmail.com>
2024-09-13 00:28:31 +0530

    Time: 17 ms (58.48%), Space: 22.8 MB (74.36%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<char,int> um;
+        int m=9;
+        for(int i=0;i<9;i++){
+            um.clear();
+            for(int j=0;j<9;j++){
+                if(um.find(board[i][j])!=um.end()){
+                 
+                    return false;
+                }
+                if(board[i][j]!='.')
+                um[board[i][j]]=1;
+                
+            }
+        }
+        for(int i=0;i<9;i++){
+            um.clear();
+            for(int j=0;j<9;j++){
+                if(um.find(board[j][i])!=um.end()){
+                    
+                    return false;
+                }
+                if(board[j][i]!='.')
+                um[board[j][i]]=1;
+                
+            }
+        }
+        int cnt=0;
+        int k=0,start1=0,start=0,p=0;
+        um.clear();
+        while(cnt<9){
+            start=k;
+            start1=p;
+            for(int i=start;i<start+3;i++){
+                for(int j=start1;j<start1+3;j++){
+                    if(um.find(board[i][j])!=um.end()){
+                    return false;
+                }
+                if(board[i][j]!='.')
+                um[board[i][j]]=1;
+                    if(i==start+2 && j==start1+2){
+                        cnt++;
+                        um.clear();
+                       
+                       p+=3;
+                        
+                        
+                        
+                    }
+                    
+                }
+            }
+            if(cnt!=0 && cnt%3==0){
+                cout<<"pp";
+               
+                k+=3;
+                            
+                p=0;
+            }
+            
+        }
+        
+        return true;
+        
+    }
+};
