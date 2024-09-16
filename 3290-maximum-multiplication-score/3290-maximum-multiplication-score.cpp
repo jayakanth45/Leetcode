74883c74fc2231bd5dfc0a52729788766aa6a042
jayakanth45 <jayakanthreddykancharla@gmail.com>
2024-09-16 12:38:00 +0530

    Time: 167 ms (33.33%), Space: 179.2 MB (33.33%) - LeetHub

--- 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
+++ 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-     long long helper(vector<int>&a,vector<int>&b,int k,int i,vector<vector<long long>>&dp){
-        if(k==4) return 0;
-        if(i>b.size()-1) return -1e18;
-        if(dp[k][i] != -1) return dp[k][i];
-         long long take = -1e18;
-         take = (long long)a[k]*b[i] + helper(a,b,k+1,i+1,dp);
-        long long notTake = helper(a,b,k,i+1,dp);
-        
-        
-        return dp[k][i] = max(take,notTake);
-    }
+    
     long long maxScore(vector<int>& a, vector<int>& b) {
-        int n = b.size();
-        vector<vector<long long>>dp(4,vector<long long>(n,-1));
-        return helper(a,b,0,0,dp);
+        int n=a.size();
+        int m=b.size();
+        vector<vector<long long>> dp(n+1,vector<long long>(m+1,0));
+        
+        for(int i=0;i<n;i++){
+            dp[i][m]=-1e18;
+        }
+        
+        for(int k=n-1;k>=0;k--){
+            for(int i=m-1;i>=0;i--){
+                long long take = -1e18;
+         take = (long long)a[k]*b[i] + dp[k+1][i+1];
+        long long notTake = dp[k][i+1];
+        
+        
+        dp[k][i] = max(take,notTake);
+                
+                
+            }
+        }
+        return dp[0][0];
     }
 };
