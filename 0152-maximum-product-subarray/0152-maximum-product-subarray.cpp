5ffc3722fe799c47bfc9a84e7b41bb9d1f9ef18c
jayakanth45 <jayakanthreddykancharla@gmail.com>
2024-09-24 21:25:35 +0530

    Time: 67 ms (6.20%), Space: 21.3 MB (5.06%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int givesum(vector<int>& nums, int idx, int curr_max, int curr_min, int& global_max) {
+    if (idx == nums.size()) {
+        return global_max;
+    }
+
+    int num = nums[idx];
+
+    // Calculate both pick cases: multiplying current max and min by the current element
+    int pick_max = max({num, num * curr_max, num * curr_min});
+    int pick_min = min({num, num * curr_max, num * curr_min});
+
+    // Update the global max product
+    global_max = max(global_max, pick_max);
+
+    // Recursively proceed to the next element
+    return givesum(nums, idx + 1, pick_max, pick_min, global_max);
+}
+
+int maxProduct(vector<int>& nums) {
+    int global_max = nums[0];
+    return givesum(nums, 1, nums[0], nums[0], global_max);
+}
+};
