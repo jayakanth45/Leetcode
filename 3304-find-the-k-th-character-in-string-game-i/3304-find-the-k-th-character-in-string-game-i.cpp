8e0c1e66a1de8e24d847111dee557d6467d3fbf8
jayakanth45 <jayakanthreddykancharla@gmail.com>
2024-09-29 11:56:30 +0530

    Time: 3 ms (50.00%), Space: 11.1 MB (7.14%) - LeetHub

---
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        string word="a";
+        while(word.size()<k){
+            string temp=word;
+            string temp1="";
+            for(int i=0;i<temp.size();i++){
+                char c=int(temp[i])+1;
+                temp1+=c;
+                
+                
+            }
+            //cout<<temp1<<endl;
+            word+=temp1;
+            cout<<word<<endl;
+        }
+       
+        return word[k-1];
+        
+    }
+};
