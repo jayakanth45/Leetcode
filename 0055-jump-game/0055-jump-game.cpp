43368f0f5ba163945b4b65769368113ed06c038f
Jayakanth Kancharla <jayakanthreddykancharla@gmail.com>
2024-11-16 00:01:22 +0530

    Time: 473 ms (13.55%), Space: 54.2 MB (7.15%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
-   
-    bool canJump(vector<int>& nums) {
-       int last_reachable_index = nums.size()-1;
-        for(int i =  nums.size()-2;i>=0;i--){
-            if(nums[i]+i>=last_reachable_index){
-                last_reachable_index = i;
+    bool function(int index, int &n, vector<int>& nums, vector<int>& dp)
+    {
+        if (index >= n - 1) 
+        {
+            return true;
+        }
+        
+        if (dp[index] != -1)
+        {
+            return dp[index];
+        }
+
+        int maxJump = nums[index];
+        for (int i = 1; i <= maxJump; i++) 
+        {
+            if (function(index + i, n, nums, dp)) 
+            {
+                return dp[index] = true;
             }
         }
-        return last_reachable_index == 0;
+        return dp[index] = false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n,-1);
+        return function(0,n,nums,dp);
     }
 };
