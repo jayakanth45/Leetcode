25c050aa0db028e43f2fd50eece80f9da67522eb
Jayakanth Kancharla <jayakanthreddykancharla@gmail.com>
2024-11-20 20:50:31 +0530

    Time: 0 ms (100.00%), Space: 86 MB (56.57%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int five=0,ten=0;
+        int fives=0,tens=0;
         for(int i=0;i<bills.size();i++){
-            if(bills[i]==5){
-                five++;
-            }
+          if(bills[i]==5){
+              fives++;
+          }
             else if(bills[i]==10){
-                if(five>=1){
-                    five-=1;
-                    ten++;
-                }
-                else{
+                if(fives==0){
                     return false;
                 }
-            }
-            else if(bills[i]==20){
-                if(five>=1 && ten>=1){
-                    ten--;
-                five--;
-                }
-            
-            else if(five>=3){
-                five-=3;
+                fives--;
+                tens++;
             }
             else{
-                return false;
+                if(!(fives>=3) && !(tens>=1 && fives>=1)){
+                    return false;
+                }
+                else{
+                    if(tens>=1 && fives>=1){
+                        tens--;
+                        fives--;
+                    }
+                    else{
+                        fives-=3;
+                    }
+                }
             }
         }
-        }
         return true;
-        
     }
 };
