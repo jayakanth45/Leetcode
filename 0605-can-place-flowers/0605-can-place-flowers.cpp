41175089ae0bdd855975f4392ab2e5ad56be877b
Jayakanth Kancharla <jayakanthreddykancharla@gmail.com>
2024-11-22 23:38:14 +0530

    Time: 0 ms (100.00%), Space: 22.8 MB (99.76%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& bed, int n) {
+        int count=0;
+        if(n==0){
+            return true;
+        }
+        if(bed.size()==1){
+            return bed[0]==0?true:false;
+        }
+        
+        if(bed.size()>1 && bed[0]==0 && bed[1]==0){
+            bed[0]=1;
+            count=1;
+        }
+        for(int i=1;i<bed.size()-1;i++){
+            if(bed[i]==0 && bed[i-1]==0 && bed[i+1]==0){
+                count++;
+                bed[i]=1;
+            }
+        }
+        int k=bed.size();
+        if(bed[k-1]==0 && bed[k-2]==0){
+            count++;
+        }
+        return count>=n?true:false;
+    }
+};
