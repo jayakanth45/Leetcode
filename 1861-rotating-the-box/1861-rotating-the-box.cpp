27723bbf6306f89866e81be17f9cc2c12c125ddb
Jayakanth Kancharla <jayakanthreddykancharla@gmail.com>
2024-11-23 19:24:45 +0530

    Time: 335 ms (5.83%), Space: 56.4 MB (25.37%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int m=box.size();
+        int n=box[0].size();
+        int flag=0,find=0;
+        vector<vector<char>> ans(n,vector<char>(m,'.'));
+        int k=m-1;
+        for(int i=0;i<box.size();i++){
+            for(int j=0;j<box[i].size();j++){
+                ans[j][k]=box[i][j];
+            }
+            k--;
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+               if(ans[j][i]=='#'){
+                   flag=1;
+                   find=0;
+                   int p=j;
+                   p++;
+                   while(flag && p<n){
+                       if(p<n && ans[p][i]=='*'){
+                           flag=0;
+                       }
+                       else if(ans[p][i]=='.'){
+                           flag=0;
+                           find=1;
+                           ans[p][i]='#';
+                          cout<<p<<i;
+                       }
+                       else{
+                           p++;
+                       }
+                   }
+                   if(find){
+                       
+                       ans[j][i]='.';
+                   }
+                   else{
+                       //cout<<j<<i;
+                       ans[j][i]='#';
+                   }
+               }
+            }
+        }
+        
+        
+        
+        
+        
+        
+        return ans;
+    }
+};
