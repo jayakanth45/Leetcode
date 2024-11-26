54a3d279960f88892a4703e2b95b7e2e773c054b
Jayakanth Kancharla <jayakanthreddykancharla@gmail.com>
2024-11-26 14:16:15 +0530

    Time: 27 ms (5.16%), Space: 9 MB (58.00%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bulls=0;
+        int cows=0;
+        unordered_map<char,int> um;
+        for(int i=0;i<secret.size();i++){
+            if(secret[i]==guess[i]){
+                bulls++;
+            }
+            else
+            um[secret[i]]++;
+        }
+        for(int i=0;i<guess.size();i++){
+            if(um.find(guess[i])!=um.end() && um[guess[i]]>0 && secret[i]!=guess[i]){
+                
+                
+                    cout<<i;
+                    cows++;
+                    um[guess[i]]--;
+                
+            }
+        }
+        string ans="";
+        ans+=to_string(bulls)+"A"+to_string(cows)+"B";
+        return ans;
+    }
+};
