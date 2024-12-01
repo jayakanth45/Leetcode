df76202f250e89a68fe2629bca3e7888a58ba344
Jayakanth Kancharla <jayakanthreddykancharla@gmail.com>
2024-12-01 23:51:19 +0530

    Time: 0 ms (100.00%), Space: 15.7 MB (31.53%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    
-    vector<string> generateParenthesis(int n) {
-        vector<string> result;
-        backtrack(result,"", 0, 0, n);
-        return result;
-    }
-
-private:
-    void backtrack(vector<string>& result, string current, int open, int close, int max) {
-     
-        if (current.length() == max * 2) {
-            result.push_back(current);
+    void giveans(vector<string> &ans,int n,string temp,int oc,int cc){
+        if(temp.size()==2*n){
+            ans.push_back(temp);
             return;
         }
-
-       
-        if (open < max) {
-            backtrack(result, current + "(", open + 1, close, max);
+        if(oc<n){
+            
+            giveans(ans,n,temp+'(',oc+1,cc);
         }
-     
-        if (close < open) {
-            backtrack(result, current + ")", open, close + 1, max);
-        }
-        
+    
+        if(cc<oc)
+        giveans(ans,n,temp+')',oc,cc+1);
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        giveans(ans,n,"",0,0);
+        return ans;
         
     }
 };
